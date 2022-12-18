a1c28a9d3ae7d55f96b65cb5a5fc672a6b31cf1b
PetxCode <brighterdayscodelab@gmail.com>
2022-12-18 08:40:42 +0100

    sessions

--- server.ts
+++ server.ts
@@ -62,6 +62,81 @@ const protectedData = (req, res, next) => {
 //   });
 // });
 
+import { google } from "googleapis";
+import googleOAuth from "passport-google-oauth20";
+import passport from "passport";
+import schoolModel from "./model/schoolModel";
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+const GoogleStrategy = googleOAuth.Strategy;
+
+const GOOGLE_SECRET = "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";
+const GOOGLE_ID =
+  "1001238833498-cqm9f9c1mh3m1khppm3392npjalj8b4s.apps.googleusercontent.com";
+const GOOGLE_REFRESHTOKEN =
+  "1//04h7d93kXEa_mCgYIARAAGAQSNwF-L9IrRBMf9gTPHHPp4rsWwU2m6arOFmIUgpZPaL-Cov37TXIF6SM2XIoFhScTFOD1ZDaezBY";
+const GOOGLE_REDIRECT = "https://developers.google.com/oauthplayground";
+
+const oAuth = new google.auth.OAuth2(GOOGLE_ID, GOOGLE_SECRET, GOOGLE_REDIRECT);
+
+oAuth.setCredentials({ refresh_token: GOOGLE_REFRESHTOKEN });
+
+const url: string = "http:localhost:2244";
+const accessToken = oAuth.getAccessToken();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_ID,
+      clientSecret: GOOGLE_SECRET,
+      callbackURL: "http://localhost:2244/auth/google/callback",
+      // callbackURL: GOOGLE_REDIRECT,
+    },
+    // function ({ accessToken, refreshToken: GOOGLE_REFRESHTOKEN, profile, cb }) {
+    //   schoolModel.create({ googleId: profile.id }, function (err, user) {
+    //     return cb(err, user);
+    //   });
+    // }
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      schoolModel.create(
+        {
+          // _id: user,
+          name: profile.displayName,
+          email: profile.emails[0].email,
+          logo: profile.photos[0].value,
+        },
+        function (err, user) {
+          console.log(user);
+          return cb(err, user);
+        }
+      );
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/auth/google" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.status(200).json({ message: "Welcome back home" });
+    // res.redirect("/");
+  }
+);
+
 app.listen(port, () => {
   console.log("server is ready");
 });
