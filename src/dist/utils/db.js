24579bf0ce0748d59027a32acb0ed2eb3fbda2a7
PetxCode <brighterdayscodelab@gmail.com>
2022-12-26 12:52:13 +0100

    fixed

--- src/dist/utils/db.js
+++ src/dist/utils/db.js
@@ -7,8 +7,8 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const proc = dotenv_1.default.config().parsed;
 const newURL = process.env.DB;
-const LOCALDB = proc.LOCALDB;
-// const LOCALDB = proc.ONLINEDB;
+// const LOCALDB = proc.LOCALDB;
+const LOCALDB = proc.ONLINEDB;
 // const url = process.env.LOCALBD;
 mongoose_1.default.connect(LOCALDB, () => {
     console.log("database is now connected...!");
